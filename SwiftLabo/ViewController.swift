4f896517509ca2beccdf7fb28b91cf615b4f72cb
ttsutchi <tailegog@gmail.com>
2018-01-17 21:09:17 +0900

    [Add] project files

---
+++ SwiftLabo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLabo
+//
+//  Created by Taichi Tsuchida on 2018/01/17.
+//  Copyright © 2018年 Taichi Tsuchida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftLaboTests/SwiftLaboTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftLaboTests.swift
+//  SwiftLaboTests
+//
+//  Created by Taichi Tsuchida on 2018/01/17.
+//  Copyright © 2018年 Taichi Tsuchida. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftLabo
+
+class SwiftLaboTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
